76efe9862f41cfc915d38ac036025bd6dec64ad3
Wilsonator123 <dudewilson52@gmail.com>
2024-08-25 22:43:43 +0100

    Started login form

---
+++ client/src/app/login/form.jsx
@@ -0,0 +1,59 @@
+'use client'
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+export default function LoginForm()
+{
+    const form = useForm();
+
+    function onSubmit(values) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Email" {...field} />
+                            </FormControl>
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Password" type="password" {...field} />
+                            </FormControl>
+                            <div className="underline text-sm">Forgot your password?</div>
+                        </FormItem>
+                    )}
+
+                />
+
+                <Button type="submit">Sign in</Button>
+            </form>
+        </Form>
+    )
+}
---
+++ client/src/app/login/page.jsx
@@ -0,0 +1,11 @@
+import LoginForm from './form'
+export default function Page(){
+
+    return (
+        <>
+            <div className="w-1/3">
+                <LoginForm />
+            </div>
+        </>
+    )
+}
